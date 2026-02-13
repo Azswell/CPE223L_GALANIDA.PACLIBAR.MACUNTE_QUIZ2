10dcf091a6313c5dee7fe9077210bb0b85eb0d52
Azswell <kurt.galanida@gmail.com>
2026-02-13 09:56:16 +0800

    completed but not working

--- EquipmentTracker.Infrastructure/AppDbContext.cs
+++ EquipmentTracker.Infrastructure/AppDbContext.cs
@@ -1,7 +1,16 @@
-﻿namespace EquipmentTracker.Infrastructure
+﻿// AppDbContext.cs
+using EquipmentTracker.Domain;
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    public DbSet<Equipment> Equipments { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 {
     public class AppDbContext
     {
-
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=AppDbReleased;Integrated Security=True;TrustServerCertificate=True;");
+        }
     }
 }
