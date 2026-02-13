e0633a967db37eb04c5e5e888841622a2deb6c90
Azswell <kurt.galanida@gmail.com>
2026-02-13 08:24:18 +0800

    Add project files.

---
+++ CPE223L_GALANIDA.PACLIBAR.MACUNTE_QUIZ2/Form1.cs
@@ -0,0 +1,10 @@
+namespace CPE223L_GALANIDA.PACLIBAR.MACUNTE_QUIZ2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
